25fe658092c7d221f92b7abe38c5d071a4156fee
aakashsingh0044-create <aakashsingh0044@gmail.com>
2026-05-28 04:55:47 +0530

    Update portfolio.js

--- js/portfolio.js
+++ js/portfolio.js
@@ -19,12 +19,3 @@ revealSections();
 
 
 
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navLinks.classList.toggle("active");
-
-  document.body.classList.toggle("menu-open");
-});
