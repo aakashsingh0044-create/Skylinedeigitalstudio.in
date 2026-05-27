e6ece98f9b2ec1ffa2a9f0164c30f1deac50f70a
aakashsingh0044-create <aakashsingh0044@gmail.com>
2026-05-28 04:55:39 +0530

    Update contact.js

--- js/contact.js
+++ js/contact.js
@@ -24,12 +24,3 @@ revealContact();
 
 
 
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.querySelector(".nav-links");
-
-hamburger.addEventListener("click", () => {
-  hamburger.classList.toggle("active");
-  navLinks.classList.toggle("active");
-
-  document.body.classList.toggle("menu-open");
-});
